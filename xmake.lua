287e4a591d0d9943908e08fa62c6ecb026c6a3b3
Kong <xykong421@outlook.com>
2024-02-07 17:25:16 +0800

    Update test_xmake_clang_toolchain.yml

--- xmake.lua
+++ xmake.lua
@@ -8,17 +8,17 @@ target("static_mylib")
     add_headerfiles("mylib/mylib.hpp")
     add_includedirs("mylib", { public = true })    
 
+target("static_main")   -- success
+    set_kind("binary")
+    add_files("static_test_src/static_main.cpp")
+    add_deps("static_mylib")
+
 target("dynamic_mylib")
     set_kind("shared")
     add_files("mylib/mylib.cpp")
     add_headerfiles("mylib/mylib.hpp")
     add_includedirs("mylib", { public = true })
     add_rules("utils.symbols.export_all", { export_classes = true })
-
-target("static_main")   -- success
-    set_kind("binary")
-    add_files("static_test_src/static_main.cpp")
-    add_deps("static_mylib")
 
 target("dynamic_main")  -- failed with error [error: clang++: error: no such file or directory: '/def:build\.gens\dynamic_mylib\windows\x64\release\rules\symbols\export_all.def']
     set_kind("binary")
