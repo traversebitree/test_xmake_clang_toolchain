fb5017faa35a762d08ef1bb4dc116645c5761472
Kong <xykong421@outlook.com>
2024-02-07 19:17:39 +0800

    Update xmake.lua

--- static_test_src/static_main.cpp
+++
@@ -1,6 +0,0 @@
-#include "mylib.hpp"
-
-int main(int argc, char **argv) {
-  print_calc(3, 4);
-  return 0;
-}
--- xmake.lua
+++ xmake.lua
@@ -2,17 +2,6 @@ add_rules("mode.debug", "mode.release")
 -- add_requires("llvm")
 add_toolchains("@clang")
 
-target("static_mylib")
-set_kind("static")
-add_files("mylib/mylib.cpp")
-add_headerfiles("mylib/mylib.hpp")
-add_includedirs("mylib", { public = true })
-
-target("static_main") -- success
-set_kind("binary")
-add_files("static_test_src/static_main.cpp")
-add_deps("static_mylib")
-
 target("dynamic_mylib")
 set_kind("shared")
 add_files("mylib/mylib.cpp")
@@ -20,7 +9,7 @@ add_headerfiles("mylib/mylib.hpp")
 add_includedirs("mylib", { public = true })
 add_rules("utils.symbols.export_all", { export_classes = true })
 
-target("dynamic_main") -- failed with error [error: clang++: error: no such file or directory: '/def:build\.gens\dynamic_mylib\windows\x64\release\rules\symbols\export_all.def']
+target("main") -- failed with error []
 set_kind("binary")
-add_files("dynamic_test_src/dynamic_main.cpp")
+add_files("main.cpp")
 add_deps("dynamic_mylib")
